9bf10a40a8182ac1862f8cb78f955cff17f80f9e
Alan <alanmesquita2022@gmail.com>
2023-11-09 11:29:41 -0300

    alterações banco de dados

--- CODIGO SQL/CODIGO_BANCO_DE_DADOS.sql
+++ CODIGO SQL/CODIGO_BANCO_DE_DADOS.sql
@@ -1,18 +1,7 @@
-#!/bin/bash
+create database PAPELARIA;
+use PAPELARIA;
 
-# Solicita ao usuário que insira o nome de usuário
-read -p "Nome de Usuário do Banco de Dados: " DB_USUARIO
 
-# Solicita ao usuário que insira a senha (a senha será mascarada)
-read -s -p "Senha do Banco de Dados: " DB_SENHA
-
-echo  # Adiciona uma quebra de linha após a entrada da senha
-
-DB_NOME="PAPELARIA"
-
-SQL_SCRIPT=$(cat <<EOF
-create database $DB_NOME;
-use $DB_NOME;
 
 -- Criação da tabela 'produtos'
 CREATE TABLE tbProduto (
@@ -35,21 +24,8 @@ CREATE TABLE tbItemPedido(
     IPE_CODIGO INT AUTO_INCREMENT PRIMARY KEY,
     PED_CODIGO INT,
     PRO_CODIGO INT,
-    PRO_QUANTIDADE INT NOT NULL,
+	PRO_QUANTIDADE INT NOT NULL,
     PRO_PRECO_UNITARIO DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (PED_CODIGO) REFERENCES tbPedido(PED_CODIGO),
     FOREIGN KEY (PRO_CODIGO) REFERENCES tbProduto(PRO_CODIGO)
 );
-EOF
-)
-
-# Executa o script SQL no banco de dados
-mysql -u $DB_USUARIO -p$DB_SENHA -e "$SQL_SCRIPT"
-
-# Verifica o status da execução
-if [ $? -eq 0 ]; then
-  echo "Banco de dados e tabelas criados com sucesso em $DB_NOME."
-else
-  echo "Erro ao criar o banco de dados e tabelas em $DB_NOME."
-fi
-
